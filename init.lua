0916db7053a928372e5aac31dc5360e170780e0e
jack mcshane <jackrmcshane@gmail.com>
2023-04-29 22:54:49 -0400

    adds lazy, tmux-navigator, basic ui

--- init.lua
+++ init.lua
@@ -2,4 +2,20 @@
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
+
+require("util.ensure_lazy")
+require("lazy").setup({ -- lazy loading plugin manager
+
+  -- Functionality
+  { "christoomey/vim-tmux-navigator", lazy = false }, -- integration with tmux
+  'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
+
+  -- Aesthetics
+  require("plugins.colors"),
+  require("plugins.ui"),
+})
+
+require("core")
+
+vim.cmd [[ colorscheme kanagawa ]]
 vim.cmd [[ echom "<(\"<) <('')>> (>\")>" ]]
---
+++ lua/plugins/ui.lua
@@ -0,0 +1,26 @@
+
+return {
+  { 'nvim-lualine/lualine.nvim', opts = {
+      options = {
+        icons_enabled = false,
+        theme = 'auto',
+        component_separators = '|',
+        section_separators = '',
+      },
+    },
+  },
+
+  { -- Adds git releated signs to the gutter, as well as utilities for managing changes
+    'lewis6991/gitsigns.nvim',
+    opts = {
+      -- See `:help gitsigns.txt`
+      signs = {
+        add = { text = '+' },
+        change = { text = '~' },
+        delete = { text = '_' },
+        topdelete = { text = '‾' },
+        changedelete = { text = '~' },
+      },
+    },
+  },
+}
